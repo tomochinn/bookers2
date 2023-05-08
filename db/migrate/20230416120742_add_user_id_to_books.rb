bd87b4ce6979c392348334c9fea3dbd4a77898dd
tomochinn <122351367+tomochinn@users.noreply.github.com>
2023-05-08 05:31:48 +0000

    [Resubmit]4-bookers2

--- db/migrate/20230416120742_add_user_id_to_books.rb
+++
@@ -1,7 +0,0 @@
-class AddUserIdToBooks < ActiveRecord::Migration[6.1]
-  def change
-    add_column :books, :user_id, :integer
-    add_column :users, :email, :string
-    add_index :users, :email, unique: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_26_052820) do
+ActiveRecord::Schema.define(version: 2023_04_13_031645) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,9 +43,9 @@ ActiveRecord::Schema.define(version: 2023_04_26_052820) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,9 +55,9 @@ ActiveRecord::Schema.define(version: 2023_04_26_052820) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.string "introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
